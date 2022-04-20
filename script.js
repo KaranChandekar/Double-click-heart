eea6cc3319f6ee12832fa4fefa333366f7d22e8d
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-20 18:46:19 +0530

    add event listener

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const times = document.querySelector("#times");
 
 let clickTime = 0;
 let timesClicked = 0;
+
+loveMe.addEventListener("click", (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime();
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      createHeart(e);
+      clickTime = 0;
+    } else {
+      clickTime = new Date().getTime();
+    }
+  }
+});
